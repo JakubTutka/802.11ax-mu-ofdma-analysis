0bfbdd5ff5176b5dc5fc5a07d19411b821495df3
Jakub Tutka <kuba.tutka@wp.pl>
2022-11-07 17:35:48 +0100

    Basics of scenario#1 4STA 1AP

--- mobility-model.cc
+++ mobility-model.cc
@@ -1,4 +1,7 @@
-#include "ns3/mobility-model.h"
+#include "ns3/mobility-module.h"
+#include "ns3/double.h"
+#include "ns3/uinteger.h"
+#include "ns3/string.h"
 
 using namespace ns3;
 
@@ -7,10 +10,61 @@ main ()
 {
   // current cartesian position, speed of an obj.
   // course change notifier
+  // helper classes, place nodes, setup mobility models
 
-  // GetObject<MobilityModel>
+  /*
+    DESIGN
 
-  // początkowa pozycja -> PositionAllocator, używamy MobilityHelper
+    - mobility models, position allocators, helper functions
+    
+    a) mobility models
+    - evolution of postition in cartesian coordinate system
+    - GetObject<MobilityModel> on Node
 
-  // Coordinate system
+    b) position allocators
+    - initial position of object
+
+    c) helper classes
+    - MobilityHelper -> {MobilityModel, PositionAllocator} on NodeContainer
+  */
+
+  /*
+    Coordinate system
+    - cartesian only
+
+    Coodinates
+    - ns3::Vector
+    - also Rectangle, Box, Waypoint
+
+    MobilityModel
+    - GetPosition(), GetDistanceFrom()
+    - position/velocity attribiutes
+    - CourseChange notifi.
+
+    Subclasses of MobilityModel:
+    -ConstantPosition/Velocity/Acceleration
+    -RandomDirection2D/Walk2D
+    - ... more
+
+    PositionAllocators:
+    - ListPositionAllocator
+    - GridPositionAllocator
+    - ...
+
+  */
+
+  MobilityHelper mobility;
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator", "MinX", DoubleValue (0.0), "MinY",
+                                 DoubleValue (0.0), "DeltaX", DoubleValue (5.0), "DeltaY",
+                                 DoubleValue (10.0), "GridWidth", UintegerValue (3), "LayoutType",
+                                 StringValue ("RowFirst"));
+
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel", "Bounds",
+                             RectangleValue (Rectangle (-50, 50, -50, 50)));
+  
+  NodeContainer wifiStaNodes;
+  wifiStaNodes.Create (10);
+
+  mobility.Install (wifiStaNodes);
+  return 0;
 }
--- scenario-1.cc
+++ scenario-1.cc
@@ -14,3 +14,102 @@
  *
  * Author: Jakub Tutka <jtutka@student.agh.edu.pl>
  */
+
+#include <iostream>
+#include <string>
+#include "ns3/command-line.h"
+#include "ns3/core-module.h"
+#include "ns3/wifi-acknowledgment.h"
+#include "ns3/node-container.h"
+#include "ns3/wifi-mac-helper.h"
+#include "ns3/wifi-helper.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/mobility-module.h"
+
+// TODO; DESCRIPTION OF SIMULATION
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("SCENARIO #1");
+
+int
+main (int argc, char *argv[])
+{
+  //   uint32_t payloadSize = 1472;
+  //   double simulationTime = 20.0;
+  uint32_t nSta = 4;
+  uint32_t nAp = 1;
+  std::string dlAckSeqType = "ACK-SU";
+  int mcs = 1;
+  int gi = 800; // ns
+
+  CommandLine cmd (__FILE__);
+  cmd.AddValue ("dlAckType", "Ack sequence type for DL OFDMA (ACK-SU, MU-BAR, AGGR-MU-BAR)",
+                dlAckSeqType);
+  cmd.AddValue ("mcs", "MCS (0-11) for simulation", mcs);
+  cmd.AddValue ("gi", "Guard Interval value (800ns, 1600ns or 3200ns)", gi);
+
+  if (dlAckSeqType == "ACK-SU")
+    {
+      Config::SetDefault ("ns3::WifiDefaultAckManager::DlMuAckSequenceType",
+                          EnumValue (WifiAcknowledgment::DL_MU_BAR_BA_SEQUENCE));
+    }
+  else if (dlAckSeqType == "MU-BAR")
+    {
+      Config::SetDefault ("ns3::WifiDefaultAckManager::DlMuAckSequenceType",
+                          EnumValue (WifiAcknowledgment::DL_MU_TF_MU_BAR));
+    }
+  else if (dlAckSeqType == "AGGR-MU-BAR")
+    {
+      Config::SetDefault ("ns3::WifiDefaultAckManager::DlMuAckSequenceType",
+                          EnumValue (WifiAcknowledgment::DL_MU_AGGREGATE_TF));
+    }
+
+  NodeContainer wifiStaNodes;
+  NodeContainer wifiApNode;
+
+  NetDeviceContainer staDevices;
+  NetDeviceContainer apDevice;
+
+  wifiStaNodes.Create (nSta);
+  wifiApNode.Create (nAp);
+
+  WifiHelper wifi;
+  WifiMacHelper mac;
+  YansWifiPhyHelper phy;
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+
+  std::ostringstream oss;
+  oss << "HeMcs" << mcs;
+
+  wifi.SetStandard (WIFI_STANDARD_80211ax);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode",
+                                StringValue (oss.str ()), "ControlMode", StringValue (oss.str ()));
+  phy.SetChannel (channel.Create ());
+  phy.Set ("ChannelSettings", StringValue ("{0, 20, BAND_5GHZ, 0}"));
+  phy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
+
+  Ssid ssid = Ssid ("scenario-1");
+
+  mac.SetType ("ns3::StaWifiMac", "Ssid", SsidValue (ssid));
+  staDevices = wifi.Install (phy, mac, wifiStaNodes);
+
+  mac.SetType ("ns3::ApWifiMac", "EnableBeaconJitter", BooleanValue (false), "Ssid",
+               SsidValue (ssid));
+  apDevice = wifi.Install (phy, mac, wifiApNode);
+
+  Config::Set ("/NodeList/*/DeviceList/*/$ns3::WifiNetDevice/HeConfiguration/GuardInterval",
+               TimeValue (NanoSeconds (gi)));
+
+  MobilityHelper mobility;
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
+
+  positionAlloc->Add (Vector (0.0, 0.0, 0.0)); // STA1
+  positionAlloc->Add (Vector (0.0, 6.0, 0.0)); // STA2
+  positionAlloc->Add (Vector (6.0, 0.0, 0.0)); // STA3
+  positionAlloc->Add (Vector (6.0, 6.0, 0.0)); // STA4
+  positionAlloc->Add (Vector (3.0, 3.0, 0.0)); // AP
+
+  return 0;
+}
